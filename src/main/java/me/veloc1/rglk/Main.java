49762bb31d2bf2c0c19038add628228c3ed2e9f5
Pavel <useless.dinosaur@veloc1.me>
2016-11-10 00:10:19 +0300

    Initial java commit with hello world

---
+++ src/main/java/me/veloc1/rglk/Main.java
@@ -0,0 +1,13 @@
+package me.veloc1.rglk;
+
+import java.io.IOException;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		return;
+	}
+
+}
